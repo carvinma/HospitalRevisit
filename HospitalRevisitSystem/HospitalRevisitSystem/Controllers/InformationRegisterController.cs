4754de87f6e95f5951bdf88343019725c482d965
jqaizj1990 <jqaizj1990@163.com>
2014-02-09 16:35:36 -0800

    InformationRegisterController新增排序功能

--- HospitalRevisitSystem/HospitalRevisitSystem/Controllers/InformationRegisterController.cs
+++ HospitalRevisitSystem/HospitalRevisitSystem/Controllers/InformationRegisterController.cs
@@ -36,6 +36,7 @@ namespace HospitalRevisitSystem.Controllers
             {
                 query = query.Where(s => s.Consulting_Phone_Number.ToLower().Contains(searchString.ToLower()) == true);
             }
+            query = query.OrderByDescending(s => s.Information_Register_ID);
             int pageSize = 10;
             int pageNumber = (page ?? 1);
             return View(query.ToPagedList(pageNumber, pageSize));
