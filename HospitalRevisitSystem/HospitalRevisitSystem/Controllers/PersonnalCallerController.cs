f439f264dc4dcfa3177082d3e6cce67badbb804c
jqaizj1990 <jqaizj1990@163.com>
2014-02-09 16:38:27 -0800

    PersonnalCallerController新增排序功能

--- HospitalRevisitSystem/HospitalRevisitSystem/Controllers/PersonnalCallerController.cs
+++ HospitalRevisitSystem/HospitalRevisitSystem/Controllers/PersonnalCallerController.cs
@@ -36,6 +36,7 @@ namespace HospitalRevisitSystem.Controllers
             {
                 query = query.Where(s => s.Remarks.ToLower().Contains(searchString.ToLower()) == true);
             }
+            query = query.OrderByDescending(s => s.Personnal_Caller_ID);
             int pageSize = 10;
             int pageNumber = (page ?? 1);
             return View(query.ToPagedList(pageNumber, pageSize));
