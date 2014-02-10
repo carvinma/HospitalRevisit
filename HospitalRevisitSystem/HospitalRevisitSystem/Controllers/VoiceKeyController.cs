86af0031d00ccf91fab6f14ffb21e4423070aa92
jqaizj1990 <jqaizj1990@163.com>
2014-02-09 16:42:48 -0800

    VoiceKeyController新增排序功能

--- HospitalRevisitSystem/HospitalRevisitSystem/Controllers/VoiceKeyController.cs
+++ HospitalRevisitSystem/HospitalRevisitSystem/Controllers/VoiceKeyController.cs
@@ -36,6 +36,7 @@ namespace HospitalRevisitSystem.Controllers
             {
                 query = query.Where(s => s.Answer_Marked_No.ToLower().Contains(searchString.ToLower()) == true);
             }
+            query = query.OrderByDescending(s => s.Voice_Key_ID);
             int pageSize = 10;
             int pageNumber = (page ?? 1);
             return View(query.ToPagedList(pageNumber, pageSize));
