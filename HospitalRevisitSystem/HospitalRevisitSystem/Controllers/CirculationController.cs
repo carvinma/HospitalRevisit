134dd2a3cfcbe8ca489f60929c8e33672dfca1a8
jqaizj1990 <jqaizj1990@163.com>
2014-02-09 16:33:16 -0800

    CirculationController新增排序功能

--- HospitalRevisitSystem/HospitalRevisitSystem/Controllers/CirculationController.cs
+++ HospitalRevisitSystem/HospitalRevisitSystem/Controllers/CirculationController.cs
@@ -36,6 +36,7 @@ namespace HospitalRevisitSystem.Controllers
             {
                 query = query.Where(s => s.tbDepartment.Department_Name.ToLower().Contains(searchString.ToLower()) == true);
             }
+            query = query.OrderByDescending(s => s.Circulation_ID);
             int pageSize = 10;
             int pageNumber = (page ?? 1);
             return View(query.ToPagedList(pageNumber, pageSize));
