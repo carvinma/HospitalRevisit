ceca7b17bc7527ef66b6a40f9a88e0c54bd7dc09
jqaizj1990 <jqaizj1990@163.com>
2014-02-09 16:39:26 -0800

    PointOutController新增排序功能

--- HospitalRevisitSystem/HospitalRevisitSystem/Controllers/PointOutController.cs
+++ HospitalRevisitSystem/HospitalRevisitSystem/Controllers/PointOutController.cs
@@ -36,6 +36,7 @@ namespace HospitalRevisitSystem.Controllers
             {
                 query = query.Where(s => s.Department_Response_Content.ToLower().Contains(searchString.ToLower()) == true);
             }
+            query = query.OrderByDescending(s => s.Point_Out_ID);
             int pageSize = 10;
             int pageNumber = (page ?? 1);
             return View(query.ToPagedList(pageNumber, pageSize));
