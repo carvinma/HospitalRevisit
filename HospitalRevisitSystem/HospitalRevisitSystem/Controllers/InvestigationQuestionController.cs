7cae13a492581fbbc68647b16dbd43710372f292
jqaizj1990 <jqaizj1990@163.com>
2014-02-09 16:36:34 -0800

    InvestigationQuestionController新增排序功能

--- HospitalRevisitSystem/HospitalRevisitSystem/Controllers/InvestigationQuestionController.cs
+++ HospitalRevisitSystem/HospitalRevisitSystem/Controllers/InvestigationQuestionController.cs
@@ -36,7 +36,7 @@ namespace HospitalRevisitSystem.Controllers
             {
                 query = query.Where(s => s.Review_Content.ToLower().Contains(searchString.ToLower()) == true);
             }
-            query = query.OrderBy(s => s.Investigation_Question_ID);
+            query = query.OrderByDescending(s => s.Investigation_Question_ID);
             int pageSize = 10;
             int pageNumber = (page ?? 1);
             return View(query.ToPagedList(pageNumber, pageSize));
