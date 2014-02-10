52f4d69f1f44e14bff49d2bb30027185c6b9ed88
jqaizj1990 <jqaizj1990@163.com>
2014-02-09 16:40:40 -0800

    RoleController新增排序功能

--- HospitalRevisitSystem/HospitalRevisitSystem/Controllers/RoleController.cs
+++ HospitalRevisitSystem/HospitalRevisitSystem/Controllers/RoleController.cs
@@ -36,6 +36,7 @@ namespace HospitalRevisitSystem.Controllers
             {
                 query = query.Where(s => s.Role_Name.ToLower().Contains(searchString.ToLower()) == true);
             }
+            query = query.OrderByDescending(s => s.Role_ID);
             int pageSize = 10;
             int pageNumber = (page ?? 1);
             return View(query.ToPagedList(pageNumber, pageSize));
