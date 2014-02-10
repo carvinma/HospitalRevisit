b632d4b5b15235f3a59dbecccad0013eac89b5a9
jqaizj1990 <jqaizj1990@163.com>
2014-02-09 16:37:32 -0800

    MessageSendPlanController新增排序功能

--- HospitalRevisitSystem/HospitalRevisitSystem/Controllers/MessageSendPlanController.cs
+++ HospitalRevisitSystem/HospitalRevisitSystem/Controllers/MessageSendPlanController.cs
@@ -36,6 +36,7 @@ namespace HospitalRevisitSystem.Controllers
             {
                 query = query.Where(s => s.Phone_Number.ToLower().Contains(searchString.ToLower()) == true);
             }
+            query = query.OrderByDescending(s => s.Message_Send_Plan_ID);
             int pageSize = 10;
             int pageNumber = (page ?? 1);
             return View(query.ToPagedList(pageNumber, pageSize));
